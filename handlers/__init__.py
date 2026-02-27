7e4f07dd745dedeb69e6380fb4f22a23222c7454
77agbox <viktoria.metod@gmail.com>
2026-02-27 15:51:59 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,15 @@
+from .start import router as start_router
+from .support import router as support_router
+from .packages import router as packages_router
+from .admin import router as admin_router
+from .clubs import router as clubs_router
+from .masterclasses import router as masterclasses_router
 
+
+def register_handlers(dp):
+    dp.include_router(start_router)
+    dp.include_router(support_router)
+    dp.include_router(packages_router)
+    dp.include_router(admin_router)
+    dp.include_router(clubs_router)
+    dp.include_router(masterclasses_router)
