799682a4fdd895a692264c04406250ee4850f438
77agbox <viktoria.metod@gmail.com>
2026-02-27 15:46:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,21 @@
+import logging
+import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.fsm.executor import start_polling  # Новый импорт
 from config import BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)  # Новый формат
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
+# Регистрация обработчиков
+from handlers import register_handlers
+register_handlers(dp)
+
+# Основная функция для запуска бота
+async def main():
+    await dp.start_polling()
+
 if __name__ == '__main__':
-    from handlers import register_handlers
-    register_handlers(dp)
-    start_polling(dp, skip_updates=True)  # Новый формат
+    asyncio.run(main())
