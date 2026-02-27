82420831c1589fbdb4fd0c45c26b4c2a3ece99d7
77agbox <viktoria.metod@gmail.com>
2026-02-27 15:44:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-from aiogram import Bot, Dispatcher
-from aiogram import types
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.executor import start_polling  # Новый импорт
 from config import BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN, parse_mode=types.HTML)
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)  # Новый формат
 dp = Dispatcher(bot)
 
 if __name__ == '__main__':
     from handlers import register_handlers
     register_handlers(dp)
-    executor.start_polling(dp, skip_updates=True)
+    start_polling(dp, skip_updates=True)  # Новый формат
